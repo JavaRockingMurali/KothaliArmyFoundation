60e30febd2e9e2162f0a74d104d52d2c4f744138
MURALI <MURALI@DESKTOP-H17S0TG>
2018-11-25 12:14:08 +0530

    properties file changed

--- KothaliArmyFoundation/src/main/java/com/mk/KothaliArmyFoundationController/HomeController.java
+++ KothaliArmyFoundation/src/main/java/com/mk/KothaliArmyFoundationController/HomeController.java
@@ -1,5 +1,23 @@
 package com.mk.KothaliArmyFoundationController;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="/Kothali")
 public class HomeController {
+	
+	@RequestMapping(value="/home",method=RequestMethod.GET)
+	public ModelAndView getHomePage(HttpServletRequest request) {
+		ModelAndView mav=new ModelAndView("KothaliHomePage");
+		return mav;
+		
+	}
+	
+	
 
 }
